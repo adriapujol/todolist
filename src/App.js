3216c12bfb051d21800eb041f4b0bff3fda98eb6
Adria Pujol <adriapujolaparicio@gmail.com>
2020-07-30 21:05:47 +0200

    basic code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Component } from 'react';
 import './App.css';
+import uuid from 'react-uuid';
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      inputTodo: "",
+      currentTodo: {
+        id: "",
+        open: "",
+        task: ""
+      }
+    }
+  }
+
+  handleTodoInput = (e) => {
+    this.setState({
+      inputTodo:  e.target.value
+    })
+  }
+
+  addTodoState = () => {
+    this.setState({
+      currentTodo: {
+        id: uuid(),
+        open: new Date(),
+        task: this.state.inputTodo
+      },
+      todos: [this.state.currentTodo, ...this.state.todos]
+    })
+
+  }
+
+  // addTodo = () => {
+  //   this.setState({
+  //     currentTodo: {
+  //       id: uuid(),
+  //       open: new Date(),
+  //       task: this.state.inputTodo
+  //     },
+  //     todos: [this.state.currentTodo, ...this.state.todos]
+  //   })
+
+  // }
+
+  addTodo = () => {
+    this.setState({
+      todos: [this.state.currentTodo, ...this.state.todos]
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <h1>ToDos</h1>
+        <div className="todo-input-box">
+          <input type="text" placeholder="Write Todo here" name="todo" value={this.state.inputTodo} onChange={this.handleTodoInput}></input>
+          <button onClick={this.addTodoState} >Todo Into State</button>
+          <button onClick={this.addTodo} >Add</button>
+        </div>
       </header>
+      <div className="todos">
+        hi
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
